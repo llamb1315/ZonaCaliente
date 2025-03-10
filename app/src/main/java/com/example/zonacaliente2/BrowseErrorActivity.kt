6c21b21f3d1cd42c33552ccb4ff882c2ba5ca026
Sebastian Llambi <sebastianllambi@gmail.com>
2025-03-09 21:51:32 -0400

    Initial commit

---
+++ app/src/main/java/com/example/zonacaliente2/BrowseErrorActivity.kt
@@ -0,0 +1,24 @@
+package com.example.zonacaliente2
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
+/**
+ * BrowseErrorActivity shows how to use ErrorFragment.
+ */
+class BrowseErrorActivity : FragmentActivity() {
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/zonacaliente2/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.zonacaliente2
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+/**
+ * Loads [MainFragment].
+ */
+class MainActivity : FragmentActivity() {
+
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main) // Reemplaza tu_archivo_xml
+        }
+
+
+}
